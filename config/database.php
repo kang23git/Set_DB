c7465e7b3e4e9bc1cbc4dd00f418e01e538c36af
shreya <sk2645@njit.edu>
2018-10-19 20:58:48 -0400

    FIX: Database.php for heroku

--- config/database.php
+++ config/database.php
@@ -83,6 +83,18 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'pg-heroku' => [
+            'driver' => 'pgsql',
+            'host' => $heroku_db_url['host'],
+            'database' => substr($heroku_db_url['path'], 1),
+            'username' => $heroku_db_url['user'],
+            'password' => $heroku_db_url['pass'],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+        ],
+
+
     ],
 
     /*
@@ -129,15 +141,5 @@ return [
 
     ],
 
-    'pg-heroku' => [
-        'driver' => 'pgsql',
-        'host' => $heroku_db_url['host'],
-        'database' => substr($heroku_db_url['path'], 1),
-        'username' => $heroku_db_url['user'],
-        'password' => $heroku_db_url['pass'],
-        'charset' => 'utf8',
-        'prefix' => '',
-        'schema' => 'public',
-    ],
 
 ];
